812b53eb4e0d4e62e14d15a54ba8d7444f26d3e7
SR <83490726+SR-0918@users.noreply.github.com>
2021-09-08 23:33:46 +0900

    contact_controller notchanged

---
+++ contacts_controller.rb
@@ -0,0 +1,37 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(
+        **contact_params,
+        name: params[:name],
+        message: params[:message],
+        url: params[:url],
+        title: params[:title],
+        service: params[:service],
+        enviroment: params[:enviroment],
+        operation: params[:operation],
+        email: params[:email]
+    )
+    if @contact.save
+        ContactMailer.contact_mail(@contact).deliver
+        flash[:notice] = "送信しました"
+        redirect_to("/contact")
+    else
+        flash[:notice] = "送信に失敗しました"
+        redirect_to("/contact")
+    end
+  end
+
+  def contact_template
+  end
+
+  private
+
+  def contact_params
+    params.permit(:cname, :cmessage, :url, :title, :service, :enviroment, :operation, :email)
+  end
+
+end
