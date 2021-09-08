d7ed90e20095dfc9d546b49ac24d92432d822cf7
SR <83490726+SR-0918@users.noreply.github.com>
2021-09-09 01:37:49 +0900

    contact_controller fix no removecolumns
    environmentカラムをremoveして新しいカラムを追加する前のファイル

--- contacts_controller.rb
+++ contacts_controller.rb
@@ -1,37 +1,29 @@
 class ContactsController < ApplicationController
-  def new
-    @contact = Contact.new
-  end
-
-  def create
-    @contact = Contact.new(
-        **contact_params,
-        name: params[:name],
-        message: params[:message],
-        url: params[:url],
-        title: params[:title],
-        service: params[:service],
-        enviroment: params[:enviroment],
-        operation: params[:operation],
-        email: params[:email]
-    )
-    if @contact.save
-        ContactMailer.contact_mail(@contact).deliver
-        flash[:notice] = "送信しました"
-        redirect_to("/contact")
-    else
-        flash[:notice] = "送信に失敗しました"
-        redirect_to("/contact")
+    def new
+      @contact = Contact.new
     end
+  
+    def create
+      @contact = Contact.new(
+         contact_params
+      )
+      if @contact.save
+          ContactMailer.contact_mail(@contact).deliver
+          flash[:notice] = "送信しました"
+          redirect_to("/contacts")
+      else
+          flash[:notice] = "送信に失敗しました"
+          redirect_to("/contacts")
+      end
+    end
+  
+    def contact_template
+    end
+  
+    private
+  
+    def contact_params
+      params.permit(:name, :message, :url, :title, :service, :operation, :email)
+    end
+  
   end
-
-  def contact_template
-  end
-
-  private
-
-  def contact_params
-    params.permit(:name, :message, :url, :title, :service, :enviroment, :operation, :email)
-  end
-
-end
